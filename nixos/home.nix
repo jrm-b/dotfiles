97a4c27fe2f70e9932151281d717ef1fae8f7a0e
jeremy <jeremy.boubee@epitech.eu>
2025-12-13 21:01:35 +0100

    🔧 nixos(home): add link to differents files

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,5 +3,29 @@
 {
   home.username = "jeremy";
   home.homeDirectory = "/home/jeremy";
+
+  home.file = {
+    ".bashrc" = ../bashrc;
+    ".profile" = ../profile;
+    ".inputrc" = ../inputrc;
+
+    ".config/starship.toml" = ../starship.toml;
+
+    ".config/hypr/hyprland.conf" = ../hypr/hyprland.conf;
+    ".config/hypr/hyprpaper.conf" = ../hypr/hypraper.conf;
+    ".config/hypr/moy.conf" = ../hypr/monitors.conf;
+    ".config/hypr/workspaces.conf" = ../hypr/workspaces.conf;
+
+    ".config/kitty/kitty.conf" = ../kitty/kitty.conf;
+    ".config/kitty/current-theme.conf" = ../kitty/current-theme.conf;
+
+    ".config/nvim/init.lua" = ../nvim/init.lua;
+    ".config/nvim/lua/config/lazy.lua" = ../nvim/lua/config/lazy.lua;
+    ".config/nvim/lua/plugins/colorscheme.lua" = ../nvim/lua/plugins/colorscheme.lua;
+    ".config/nvim/lua/plugins/lsp.lua" = ../nvim/lua/plugins/lsp.lua;
+    ".config/nvim/lua/plugins/telescope.lua" = ../nvim/lua/plugins/telescope.lua;
+    ".config/nvim/lua/plugins/treesitter.lua" = ../nvim/lua/plugins/treesitter.lua;
+  };
+
   home.stateVersion = "25.11";
 }
